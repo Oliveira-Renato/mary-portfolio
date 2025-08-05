7987aeb1a7a0601cb61f64630573650b67531f62
Renato  Oliveira <81115873+Oliveira-Renato@users.noreply.github.com>
2025-08-05 09:26:33 -0300

    Update videos.ts
    using json videos

--- src/assets/videos/videos.ts
+++ src/assets/videos/videos.ts
@@ -1,12 +1,12 @@
-import video1 from "@/videos/fashion-film-aversion.mp4";
-import video2 from "@/videos/fashion-film-bomb-b.mp4";
-import video3 from "@/videos/fashion-film-bomb.mp4";
+import video1 from "@/videos/fashion-film-aversion.mp4.json";
+import video2 from "@/videos/fashion-film-bomb-b.mp4.json";
+import video3 from "@/videos/fashion-film-bomb.mp4.json";
 import video4 from "@/videos/fashion-film-booq.mp4.json";
 
-import videoIn1 from "@/videos/interativos/interativo-aversion.mp4";
-import videoIn2 from "@/videos/interativos/interativo-filtran-andorinha-2.mp4";
-import videoIn3 from "@/videos/interativos/interativo-filtran-andorinha.mp4";
-import videoIn4 from "@/videos/interativos/interativo-filtran.mp4";
+import videoIn1 from "@/videos/interativos/interativo-aversion.mp4.json";
+import videoIn2 from "@/videos/interativos/interativo-filtran-andorinha-2.mp4.json";
+import videoIn3 from "@/videos/interativos/interativo-filtran-andorinha.mp4.json";
+import videoIn4 from "@/videos/interativos/interativo-filtran.mp4.json";
 
 const FASHIONFILMES = {
   1: video1,
